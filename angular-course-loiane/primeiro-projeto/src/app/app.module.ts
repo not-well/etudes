b823c6e123cf6bdedf1c78635011ee17847e4a00
Wellvergton Marques <wmc.jp@outlook.com>
2022-04-04 22:29:54 -0300

    Lesson 5

--- angular-course-loiane/primeiro-projeto/src/app/app.module.ts
+++ angular-course-loiane/primeiro-projeto/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CursosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
