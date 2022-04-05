7f461409c0c8266273aa2be433d1c6e5e51efc1d
Wellvergton Marques <wmc.jp@outlook.com>
2022-04-04 22:55:46 -0300

    Lesson 6

--- angular-course-loiane/primeiro-projeto/src/app/cursos/cursos.component.ts
+++ angular-course-loiane/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Agular', 'Node'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.nomePortal = 'https://loiane.training/';
   }
 
+  ngOnInit(): void {}
 }
