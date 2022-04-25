143306145c76a82e68f59e48319a2683057b4d42
Wellvergton Marques <wmc.jp@outlook.com>
2022-04-25 20:50:29 -0300

    Lesson 12

--- angular-course-loiane/data-binding/src/app/app.module.ts
+++ angular-course-loiane/data-binding/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeuFormComponent } from './meu-form/meu-form.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataBindingComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DataBindingComponent, MeuFormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-course-loiane/data-binding/src/app/meu-form/meu-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-form',
+  templateUrl: './meu-form.component.html',
+  styleUrls: ['./meu-form.component.css'],
+})
+export class MeuFormComponent implements OnInit {
+  constructor() {}
+
+  nome: string = 'abc';
+  pessoa = {
+    nome: 'def',
+    idade: 20,
+  };
+
+  ngOnInit(): void {}
+}
