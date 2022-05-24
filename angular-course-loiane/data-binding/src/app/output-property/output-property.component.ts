c1cfd5a7063eaf38d80fda886660b6bc022b8bcf
Wellvergton Marques <wmc.jp@outlook.com>
2022-05-23 23:19:47 -0300

    Lesson 16

--- angular-course-loiane/data-binding/src/app/output-property/output-property.component.ts
+++ angular-course-loiane/data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -14,15 +22,18 @@ export class OutputPropertyComponent implements OnInit {
   @Output()
   mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput')
+  valorCampoInput: ElementRef = {} as ElementRef;
+
   ngOnInit(): void {}
 
   incrementa(): void {
-    ++this.valor;
+    ++this.valorCampoInput.nativeElement.value;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa(): void {
-    --this.valor;
+    --this.valorCampoInput.nativeElement.value;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 }
