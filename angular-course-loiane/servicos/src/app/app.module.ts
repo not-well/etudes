487f380fdd19d82bc591747c84d60e84c6d4fb45
Wellvergton Marques <wmc.jp@outlook.com>
2022-11-16 20:42:55 -0300

    Lesson 42

--- angular-course-loiane/servicos/src/app/app.module.ts
+++ angular-course-loiane/servicos/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
+import { LogService } from './shared/log.service';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CriarCursoModule, CursosModule],
-  providers: [CursosService],
+  // providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- angular-course-loiane/servicos/src/app/cursos/cursos.service.ts
+++ angular-course-loiane/servicos/src/app/cursos/cursos.service.ts
@@ -1,20 +1,24 @@
 import { EventEmitter, Injectable } from '@angular/core';
 
+import { LogService } from '../shared/log.service';
+
 @Injectable()
 export class CursosService {
   private cursos: string[] = ['Angular', 'Java', 'Spring'];
   emitirCursoCriado: EventEmitter<string> = new EventEmitter<string>();
   static criouNovoCurso: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() {
+  constructor(private logService: LogService) {
     console.log('CursosService');
   }
 
   getCursos(): string[] {
+    this.logService.consoleLog('Obtendo lista de cursos');
     return this.cursos;
   }
 
   addCurso(curso: string): void {
+    this.logService.consoleLog(`Criando um novo curso: ${curso}`);
     this.cursos.push(curso);
     this.emitirCursoCriado.emit(curso);
     CursosService.criouNovoCurso.emit(curso);
