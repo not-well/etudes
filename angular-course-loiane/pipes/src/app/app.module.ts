e71c4ccdab76b1db3a615d6cad075454bc4ee7cc
Wellvergton Marques <wmc.jp@outlook.com>
2022-11-16 22:37:55 -0300

    Lesson 45

--- angular-course-loiane/pipes/src/app/app.module.ts
+++ angular-course-loiane/pipes/src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import '@angular/common/locales/global/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplosPipesComponent,
-    CamelCasePipe
+  declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt-BR',
+    // },
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: SettingsService) =>
+        settingsService.getLocale(),
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
